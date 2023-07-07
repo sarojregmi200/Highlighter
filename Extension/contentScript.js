6ff81c3e0b7a8bc7ab057c6b23f8e15b29b072bb
saroj regmi <sarojregmi.official@gmail.com>
2023-07-07 11:53:35 +0545

    BUG::FIREFOX: content script is not working everywhere

---
+++ Extension/contentScript.js
@@ -0,0 +1,3 @@
+// this is a script that gets injected in every tab
+// it looks like it doesnot work on some pages even after providing all urls in the manifest file
+console.log("I am content script");
--- Extension/scripts/contentScript.js
+++
@@ -1,3 +0,0 @@
-// this is a script that gets injected in every tab
-
-console.log("I am content script");
