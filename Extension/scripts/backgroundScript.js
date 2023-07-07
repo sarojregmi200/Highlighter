20ddc73017f1a781cc943626797496bc7da5cad9
saroj regmi <sarojregmi.official@gmail.com>
2023-07-07 10:45:34 +0545

    Update: figuring out the scripts complete

--- Extension/scripts/backgroundScript.js
+++ Extension/scripts/backgroundScript.js
@@ -1,1 +1,7 @@
+// non-persistant script are allowed in manifest version 3
+// can send xhr request if host permission is provided
+// can communicate with conten script throught message passing api
+// non resetting context
+// all the global context should be present in this script
+
 console.log("I am background script");
--- Extension/scripts/contentScript.js
+++ Extension/scripts/contentScript.js
@@ -1,1 +1,3 @@
+// this is a script that gets injected in every tab
+
 console.log("I am content script");
