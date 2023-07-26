2461f4a7ee44a2d1fca35ec146cb45135c9e2b71
sarojregmi200 <sarojregmi.official@gmail.com>
2023-07-26 12:32:58 +0545

    Update: new folder structure with management

---
+++ Extension-ts/React/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import Test from "./test";
+function App() {
+  return (
+    <div
+      style={{
+        height: "200px",
+        width: "200px",
+        background: "black",
+      }}
+    >
+      <Test />
+    </div>
+  );
+}
+
+export default App;
