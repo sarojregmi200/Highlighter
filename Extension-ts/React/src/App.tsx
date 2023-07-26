112a33657dcd201623bedc88ee1a0b55bcf335cb
sarojregmi200 <sarojregmi.official@gmail.com>
2023-07-26 12:39:24 +0545

    Update: clean up

--- Extension-ts/React/src/App.tsx
+++ Extension-ts/React/src/App.tsx
@@ -1,17 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div
-      style={{
-        height: "200px",
-        width: "200px",
-        background: "black",
-      }}
-    >
-      hlo
-    </div>
-  );
+  return <div>Hlo Extension</div>;
 }
 
 export default App;
