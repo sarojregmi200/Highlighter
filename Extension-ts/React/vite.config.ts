1b1a7c7fe4896419b731df6449876ce8fd67d7f7
sarojregmi200 <sarojregmi.official@gmail.com>
2023-07-27 12:16:30 +0545

    Using library mode

--- Extension-ts/React/vite.config.ts
+++ Extension-ts/React/vite.config.ts
@@ -4,20 +4,27 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
     emptyOutDir: true,
     outDir: "../Extension/src",
     assetsDir: "./", // with respect to outDir,
+
+    lib: {
+      entry: {
+        popup: resolve("./src/popup/popup.tsx"),
+        background: resolve("./src/background/background.ts"),
+        contentScript: resolve("./src/contentScript/contentScript.tsx"),
+      },
+      formats: ["es"],
+      fileName: "[name]",
+    },
     rollupOptions: {
+      external: ["react"],
       output: {
         assetFileNames: "style[extname]",
-        manualChunks: {
-          popup: [resolve("./src/popup/popup.tsx")],
-          background: [resolve("./src/background/background.ts")],
-          contentScript: [resolve("./src/contentScript/contentScript.tsx")],
+        globals: {
+          react: "react",
         },
-        chunkFileNames: "scripts/[name].js",
       },
     },
   },
