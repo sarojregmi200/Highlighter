372329d8ca66612e4612d31f0bc9f6dc57b20008
sarojregmi200 <sarojregmi.official@gmail.com>
2023-07-27 11:38:48 +0545

    Update: using resolve instead of direct path string

--- Extension-ts/React/vite.config.ts
+++ Extension-ts/React/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,9 +13,9 @@ export default defineConfig({
       output: {
         assetFileNames: "style[extname]",
         manualChunks: {
-          popup: ["./src/popup/popup.tsx"],
-          background: ["./src/background/background.ts"],
-          contentScript: ["./src/contentScript/contentScript.tsx"],
+          popup: [resolve("./src/popup/popup.tsx")],
+          background: [resolve("./src/background/background.ts")],
+          contentScript: [resolve("./src/contentScript/contentScript.tsx")],
         },
         chunkFileNames: "scripts/[name].js",
       },
