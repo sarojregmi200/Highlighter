0fca045a3c994a6cb39d6a376a19addef3793fc5
sarojregmi200 <sarojregmi.official@gmail.com>
2023-08-01 02:56:38 +0545

    Update: removed the backend

--- backend/controllers/_highlighter.js
+++
@@ -1,7 +0,0 @@
-export const getAllHighlits = async (req, res) => {
-  try {
-    // fetch and return the data
-  } catch (e) {
-    res.status(500).json({ msg: "Bad request!! couldnot fetch all highlits" });
-  }
-};
--- backend/routes/highlighter.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { getAllHighlits } from "../controllers/_highlighter.js";
-// route that contains the operation realated to highlights
-const highlighterRoute = express.Router();
-
-highlighterRoute.get("/:id", getAllHighlits);
-
-export default highlighterRoute;
