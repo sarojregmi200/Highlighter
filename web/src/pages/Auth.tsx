c1a31e261bdb88af909d0ef81cbc4367daaae8de
sarojregmi200 <sarojregmi.official@gmail.com>
2023-08-03 11:26:10 +0545

    Update: cookie setup complete

--- web/src/pages/Auth.tsx
+++ web/src/pages/Auth.tsx
@@ -10,17 +10,29 @@ function AuthPage() {
     const type = event.payload.event;
     switch (type) {
       case "signIn":
+        document.cookie =
+          "authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie =
+          " userId=;   expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie =
+          " activeId=;  expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        document.cookie =
+          " settingsId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
         setCookie(event.payload.data.signInUserSession.accessToken.jwtToken);
         navigate("/home");
         break;
     }
   });
 
-  function setCookie(userName: string) {
+  function setCookie(token: string) {
+    console.log({
+      token,
+    });
     const date = new Date();
     date.setTime(date.getTime() + 3 * 24 * 60 * 60 * 1000);
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = "user=" + userName + ";" + expires + ";path=/";
+    const expires = date.toUTCString();
+    document.cookie = `authToken=${token}; expires=${expires}; path=/`;
   }
 
   return (
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -3,35 +3,56 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { createActive, createSettings, createUser } from "../graphql/mutations";
+import { listUsers } from "../graphql/queries";
 
 function Home() {
   const navigate = useNavigate();
   const signout = () => {
-    document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie =
+      "authToken=; userId=; activeId=; settingsId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie =
+      " userId=;   expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie =
+      " activeId=;  expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie =
+      " settingsId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
     Auth.signOut().then(() => {
       navigate("/");
     });
   };
-  const getCookie = (): string => {
+  const setCookie = (userId: string, activeId: string, settingsId: string) => {
+    const date = new Date();
+    date.setTime(date.getTime() + 3 * 24 * 60 * 60 * 1000);
+    const expires = "expires=" + date.toUTCString();
+
+    const authToken = getCookie("authToken");
+    console.log(userId);
+
+    document.cookie = `authToken=${authToken};expires=${expires};path=/;`;
+    document.cookie = `userId=${userId};expires=${expires};path=/;`;
+    document.cookie = `activeId=${activeId};expires=${expires};path=/;`;
+    document.cookie = `settingsId=${settingsId};expires=${expires};path=/;`;
+  };
+  const getCookie = (cookieName: string): string => {
     if (
       !document.cookie ||
       !document.cookie
         .split(";")
-        .filter((cookie) => cookie.includes("user="))[0]
+        .filter((cookie) => cookie.includes(`${cookieName}=`))[0]
     )
       return "";
     return document.cookie
       .split(";")
-      .filter((cookie) => cookie.includes("user="))[0]
-      .replace("user=", "");
+      .filter((cookie) => cookie.includes(`${cookieName}=`))[0]
+      .replace(`${cookieName}=`, "");
   };
 
-  async function addNewUser(userId: string) {
-    if (!userId) return;
+  async function addNewUser(authToken: string) {
+    if (!authToken) return;
 
     try {
-      const userSettings = await API.graphql({
+      const userSettings = (await API.graphql({
         query: createSettings,
         variables: {
           input: {
@@ -39,8 +60,8 @@ function Home() {
             topics: ["Global", "Research", "Learnings"],
           },
         },
-      });
-      const userActives = await API.graphql({
+      })) as any;
+      const userActives = (await API.graphql({
         query: createActive,
         variables: {
           input: {
@@ -51,10 +72,10 @@ function Home() {
             underline: true,
           },
         },
-      });
+      })) as any;
 
       //   creating a new user
-      await API.graphql({
+      const user = (await await API.graphql({
         query: createUser,
         variables: {
           input: {
@@ -62,17 +83,59 @@ function Home() {
             userSettingsId: userSettings?.data?.createSettings.id,
           },
         },
+      })) as any;
+
+      const { data } = user;
+
+      const dbUserId = data.createUser.id;
+      const dbUserActiveId = data.createUser.active.id;
+      const dbSettingId = data.createUser.settings.id;
+
+      console.log({
+        dbSettingId,
+        dbUserActiveId,
+        dbUserId,
       });
+
+      setCookie(dbUserId, dbUserActiveId, dbUserId);
     } catch (error) {
       console.log(error);
     }
   }
 
-  useEffect(() => {
-    const userId = getCookie();
-    if (!userId) return navigate("/auth");
+  async function checkUserExistance(): Promise<
+    { userId: string; activeId: string; settingsId: string } | false
+  > {
+    const {
+      data: {
+        listUsers: { items },
+      },
+    } = (await API.graphql({
+      query: listUsers,
+      variables: {
+        input: {},
+      },
+    })) as { data: { listUsers: { items: any } } };
+    if (!items[0] || !items[0].id) return false;
 
-    addNewUser(userId);
+    return {
+      userId: items[0].id,
+      settingsId: items[0].settings.id,
+      activeId: items[0].active.id,
+    };
+  }
+
+  useEffect(() => {
+    (async () => {
+      let authToken = getCookie("authToken");
+      if (!authToken) return navigate("/auth");
+
+      const user = await checkUserExistance();
+      if (!user) return addNewUser(authToken);
+
+      const { userId, settingsId, activeId } = user;
+      return setCookie(userId, settingsId, activeId);
+    })();
   }, []);
   return (
     <div className="mainContainer">
