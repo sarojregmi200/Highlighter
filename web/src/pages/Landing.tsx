df88896991025958794960490add28c68ad1780d
sarojregmi200 <sarojregmi.official@gmail.com>
2023-08-05 15:11:31 +0545

    Update: testing nav link

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,5 +1,12 @@
+import { NavLink } from "react-router-dom";
+
 function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div>
+      Landing
+      <NavLink to={"/home"}>Go to home</NavLink>
+    </div>
+  );
 }
 
 export default Landing;
