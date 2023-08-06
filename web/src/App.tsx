06c9ee27c67f7ceadfef19d349bf2d5a17b5d41d
sarojregmi200 <sarojregmi.official@gmail.com>
2023-08-06 20:32:29 +0545

    Build: check 3

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,41 +1,21 @@
 import { Amplify } from "aws-amplify";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
 import AuthPage from "./pages/Auth";
-import ErrorPage from "./pages/ErrorPage";
 import awsmobile from "./safeAwsExports";
 import { useEffect } from "react";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Landing />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    index: true,
-    path: "auth/",
-    element: <AuthPage />,
-    errorElement: <ErrorPage />,
-  },
-
-  {
-    path: "/home",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-]);
 
 export function App() {
   useEffect(() => {
     Amplify.configure(awsmobile);
-    console.log({ awsmobile });
   }, []);
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <Routes>
+      <Route element={<Landing />} path="/" />
+      <Route element={<Home />} path="/home" />
+      <Route element={<AuthPage />} path="/auth" />
+    </Routes>
   );
 }
 
