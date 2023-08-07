55cd2b0ebcf03ba20ed453d75c2c697fd3a25530
sarojregmi200 <sarojregmi.official@gmail.com>
2023-08-07 13:00:52 +0545

    Update: design complete without effects

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -74,95 +74,97 @@ function Landing() {
           </div>
         </div>
 
-        <div className="leftSide">
-          <div className="textContents">
-            <div className="title">Highlighter</div>
-            <div className="desc">
-              There are a lot of things that we learn everyday. Most of which
-              are very important but not for now, so we tend to pretend like you
-              will remember those things. But the reality is we never remember
-              those things, and at the time we need those we are prone to search
-              those things again. This is why you should get a highlighter and
-              research or consume information all you want and never ever forget
-              a single thing. By highlighting the important things you can
-              remember those things for longer and In case you forget those
-              things you can always back track those information with the built
-              in full text search engine. Are you ready to join the journey?
-              Let’s change the way we read on the web.
+        <div className="mainContents">
+          <div className="leftSide">
+            <div className="textContents">
+              <div className="title">Highlighter</div>
+              <div className="desc">
+                There are a lot of things that we learn everyday. Most of which
+                are very important but not for now, so we tend to pretend like
+                you will remember those things. But the reality is we never
+                remember those things, and at the time we need those we are
+                prone to search those things again. This is why you should get a
+                highlighter and research or consume information all you want and
+                never ever forget a single thing. By highlighting the important
+                things you can remember those things for longer and In case you
+                forget those things you can always back track those information
+                with the built in full text search engine. Are you ready to join
+                the journey? Let’s change the way we read on the web.
+              </div>
+            </div>
+
+            <div className="buttons">
+              <div className="downloadExtension button primary">
+                Download Extension
+              </div>
+              <div className="button secondary contribute">Contribute</div>
+            </div>
+            <div className={"goodThings"}>
+              <div className={"thing"}>
+                <Bolt />
+                <div className={"txt"}>Open Source</div>
+              </div>
+              <div className={"thing"}>
+                <Bolt />
+                <div className={"txt"}>community driven</div>
+              </div>
+              <div className={"thing"}>
+                <Bolt />
+                <div className={"txt"}>No ads</div>
+              </div>
             </div>
           </div>
 
-          <div className="buttons">
-            <div className="downloadExtension button primary">
-              Download Extension
+          <div className="rightSide">
+            <div className="mainLogo">
+              <svg
+                width="237"
+                height="237"
+                viewBox="0 0 237 237"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  cx="118.5"
+                  cy="118.5"
+                  r="118.5"
+                  fill="url(#paint0_linear_105_23)"
+                />
+                <path
+                  d="M56.2716 173.654L42.892 185.273C42.1935 185.88 42.6225 187.028 43.5477 187.028H60.0198C60.2852 187.028 60.5397 186.923 60.7272 186.735L64.4508 183.008C64.821 182.638 64.8426 182.044 64.5002 181.648L57.6841 173.756C57.3224 173.337 56.6894 173.291 56.2716 173.654Z"
+                  fill="#FFC03F"
+                />
+                <path
+                  d="M69.2894 125.304C68.8703 124.885 68.6608 124.675 68.588 124.436C68.524 124.225 68.5311 124 68.6081 123.794C68.6958 123.56 68.9181 123.364 69.3627 122.972L145.39 55.948C150.995 51.0071 153.797 48.5366 156.925 47.6801C159.679 46.9261 162.597 47.0319 165.288 47.9833C168.346 49.0641 170.962 51.731 176.194 57.0649L181.526 62.5008C186.653 67.7273 189.217 70.3405 190.243 73.3648C191.147 76.0277 191.228 78.901 190.475 81.6106C189.621 84.6878 187.208 87.4412 182.384 92.9479L153.975 125.374L127.613 155.564L116.783 167.514C115.864 168.528 114.449 168.932 113.133 168.556C112.589 168.401 112.089 168.119 111.675 167.734L106.353 162.792L93.1713 149.186L69.2894 125.304Z"
+                  fill="#FFC03F"
+                />
+                <path
+                  d="M56.7595 175.635C55.9178 174.834 55.7275 173.563 56.2973 172.55C58.1983 169.17 59.7726 165.617 60.9988 161.939L62.9116 156.2C62.9585 156.06 62.9895 155.914 63.0043 155.767L63.8 147.81C64.1044 144.766 64.1044 141.699 63.8 138.656L63.4073 134.729C63.1331 131.986 63.857 129.237 65.4463 126.986C66.8198 125.04 69.6205 124.814 71.2887 126.514L112.049 168.05C112.486 168.495 112.73 169.094 112.73 169.718L112.73 170.343C112.73 170.953 112.488 171.538 112.057 171.97C110.316 173.71 107.933 174.654 105.473 174.577L95.7225 174.272L92.8177 174.343C83.2838 174.576 73.9597 177.19 65.6949 181.949C64.8426 182.44 63.769 182.311 63.0568 181.633L56.7595 175.635Z"
+                  fill="#0C4B5C"
+                />
+                <path
+                  d="M104.486 160.514L77.7759 133.377C77.3657 132.96 76.6868 132.983 76.3053 133.426L73.3959 136.807C71.8052 138.655 70.8589 140.971 70.6998 143.404C70.3762 148.355 69.1997 153.213 67.2224 157.763L66.3211 159.837L64.134 163.867C62.0575 167.693 59.7121 171.366 57.1154 174.86C56.8244 175.252 56.874 175.8 57.2307 176.133L62.4963 181.047C62.7772 181.309 63.206 181.331 63.5123 181.099L65.5055 179.589C73.0964 173.838 81.7903 169.713 91.0455 167.469L97.5571 166.736C100.383 166.418 102.949 164.937 104.638 162.648C105.119 161.996 105.054 161.091 104.486 160.514Z"
+                  fill="#0E4353"
+                />
+                <path
+                  d="M94.8721 128.731C94.8721 123.367 97.2383 118.277 101.339 114.819L150.746 73.1546C151.477 72.538 152.297 72.0349 153.178 71.6623C160.941 68.3776 168.836 76.0438 165.781 83.9007L165.59 84.3901C165.22 85.3434 164.687 86.2257 164.017 86.9982L122.655 134.638C119.293 138.51 114.539 140.898 109.427 141.284L108.485 141.355C101.138 141.909 94.8721 136.098 94.8721 128.731Z"
+                  fill="#FDD682"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_105_23"
+                    x1="109.612"
+                    y1="66.1625"
+                    x2="297.237"
+                    y2="398.95"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stop-color="white" />
+                    <stop offset="0.963537" stop-color="#F4D085" />
+                  </linearGradient>
+                </defs>
+              </svg>
             </div>
-            <div className="button secondary contribute">Contribute</div>
-          </div>
-          <div className={"goodThings"}>
-            <div className={"thing"}>
-              <Bolt />
-              <div className={"txt"}>Open Source</div>
-            </div>
-            <div className={"thing"}>
-              <Bolt />
-              <div className={"txt"}>community driven</div>
-            </div>
-            <div className={"thing"}>
-              <Bolt />
-              <div className={"txt"}>No ads</div>
-            </div>
-          </div>
-        </div>
-
-        <div className="rightSide">
-          <div className="mainLogo">
-            <svg
-              width="237"
-              height="237"
-              viewBox="0 0 237 237"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <circle
-                cx="118.5"
-                cy="118.5"
-                r="118.5"
-                fill="url(#paint0_linear_105_23)"
-              />
-              <path
-                d="M56.2716 173.654L42.892 185.273C42.1935 185.88 42.6225 187.028 43.5477 187.028H60.0198C60.2852 187.028 60.5397 186.923 60.7272 186.735L64.4508 183.008C64.821 182.638 64.8426 182.044 64.5002 181.648L57.6841 173.756C57.3224 173.337 56.6894 173.291 56.2716 173.654Z"
-                fill="#FFC03F"
-              />
-              <path
-                d="M69.2894 125.304C68.8703 124.885 68.6608 124.675 68.588 124.436C68.524 124.225 68.5311 124 68.6081 123.794C68.6958 123.56 68.9181 123.364 69.3627 122.972L145.39 55.948C150.995 51.0071 153.797 48.5366 156.925 47.6801C159.679 46.9261 162.597 47.0319 165.288 47.9833C168.346 49.0641 170.962 51.731 176.194 57.0649L181.526 62.5008C186.653 67.7273 189.217 70.3405 190.243 73.3648C191.147 76.0277 191.228 78.901 190.475 81.6106C189.621 84.6878 187.208 87.4412 182.384 92.9479L153.975 125.374L127.613 155.564L116.783 167.514C115.864 168.528 114.449 168.932 113.133 168.556C112.589 168.401 112.089 168.119 111.675 167.734L106.353 162.792L93.1713 149.186L69.2894 125.304Z"
-                fill="#FFC03F"
-              />
-              <path
-                d="M56.7595 175.635C55.9178 174.834 55.7275 173.563 56.2973 172.55C58.1983 169.17 59.7726 165.617 60.9988 161.939L62.9116 156.2C62.9585 156.06 62.9895 155.914 63.0043 155.767L63.8 147.81C64.1044 144.766 64.1044 141.699 63.8 138.656L63.4073 134.729C63.1331 131.986 63.857 129.237 65.4463 126.986C66.8198 125.04 69.6205 124.814 71.2887 126.514L112.049 168.05C112.486 168.495 112.73 169.094 112.73 169.718L112.73 170.343C112.73 170.953 112.488 171.538 112.057 171.97C110.316 173.71 107.933 174.654 105.473 174.577L95.7225 174.272L92.8177 174.343C83.2838 174.576 73.9597 177.19 65.6949 181.949C64.8426 182.44 63.769 182.311 63.0568 181.633L56.7595 175.635Z"
-                fill="#0C4B5C"
-              />
-              <path
-                d="M104.486 160.514L77.7759 133.377C77.3657 132.96 76.6868 132.983 76.3053 133.426L73.3959 136.807C71.8052 138.655 70.8589 140.971 70.6998 143.404C70.3762 148.355 69.1997 153.213 67.2224 157.763L66.3211 159.837L64.134 163.867C62.0575 167.693 59.7121 171.366 57.1154 174.86C56.8244 175.252 56.874 175.8 57.2307 176.133L62.4963 181.047C62.7772 181.309 63.206 181.331 63.5123 181.099L65.5055 179.589C73.0964 173.838 81.7903 169.713 91.0455 167.469L97.5571 166.736C100.383 166.418 102.949 164.937 104.638 162.648C105.119 161.996 105.054 161.091 104.486 160.514Z"
-                fill="#0E4353"
-              />
-              <path
-                d="M94.8721 128.731C94.8721 123.367 97.2383 118.277 101.339 114.819L150.746 73.1546C151.477 72.538 152.297 72.0349 153.178 71.6623C160.941 68.3776 168.836 76.0438 165.781 83.9007L165.59 84.3901C165.22 85.3434 164.687 86.2257 164.017 86.9982L122.655 134.638C119.293 138.51 114.539 140.898 109.427 141.284L108.485 141.355C101.138 141.909 94.8721 136.098 94.8721 128.731Z"
-                fill="#FDD682"
-              />
-              <defs>
-                <linearGradient
-                  id="paint0_linear_105_23"
-                  x1="109.612"
-                  y1="66.1625"
-                  x2="297.237"
-                  y2="398.95"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stop-color="white" />
-                  <stop offset="0.963537" stop-color="#F4D085" />
-                </linearGradient>
-              </defs>
-            </svg>
           </div>
         </div>
       </div>
